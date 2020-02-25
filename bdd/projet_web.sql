8acc695ed94d1a7a4fdad1c3028ad5bcdfa75b26
laurent <laurent.bodin58@gmail.com>
2020-02-25 15:13:57 +0100

    BDD corrigée

--- bdd/projet_web.sql
+++ bdd/projet_web.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.3
+-- version 4.9.0.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Feb 25, 2020 at 11:55 AM
+-- Generation Time: Feb 25, 2020 at 02:12 PM
 -- Server version: 5.7.26
--- PHP Version: 7.3.9
+-- PHP Version: 7.3.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -417,29 +417,31 @@ CREATE TABLE `tempedie` (
   `numero` int(4) UNSIGNED NOT NULL,
   `nom` varchar(20) NOT NULL,
   `type_1` varchar(10) NOT NULL,
-  `type_2` varchar(10) DEFAULT NULL
+  `type_2` varchar(10) DEFAULT NULL,
+  `trait_1` varchar(20) NOT NULL,
+  `trait_2` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `tempedie`
 --
 
-INSERT INTO `tempedie` (`id`, `numero`, `nom`, `type_1`, `type_2`) VALUES
-(1, 7, 'Platypet', 'Eau', 'Toxic'),
-(2, 8, 'Platox', 'Eau', 'Toxic'),
-(3, 9, 'Platimous', 'Eau', 'Toxic'),
-(4, 10, 'Swali', 'Nature', NULL),
-(5, 11, 'Loali', 'Nature', 'Vent'),
-(6, 12, 'Tateru', 'Neutre', NULL),
-(7, 16, 'Paharo', 'Vent', NULL),
-(8, 17, 'Paharac', 'Vent', NULL),
-(9, 18, 'Granpah', 'Vent', NULL),
-(10, 21, 'Bunbun', 'Terre', 'Cristal'),
-(11, 22, 'Mudrid', 'Terre', 'Cristal'),
-(12, 23, 'Hidody', 'Nature', NULL),
-(13, 24, 'Taifu', 'Nature', NULL),
-(14, 72, 'Crystle', 'Cristal', NULL),
-(15, 73, 'Sherald', 'Cristal', NULL);
+INSERT INTO `tempedie` (`id`, `numero`, `nom`, `type_1`, `type_2`, `trait_1`, `trait_2`) VALUES
+(1, 7, 'Platypet', 'Eau', 'Toxic', 'Virulent', 'Amphibie'),
+(2, 8, 'Platox', 'Eau', 'Toxic', 'Résistant', 'Vaillant'),
+(3, 9, 'Platimous', 'Eau', 'Toxic', 'Zen', 'Déterminé'),
+(4, 10, 'Swali', 'Nature', NULL, 'Mithridate', 'Partageur'),
+(5, 11, 'Loali', 'Nature', 'Vent', 'Botaniste', 'Revanchard'),
+(6, 12, 'Tateru', 'Neutre', NULL, 'Vaillant', 'Délicat'),
+(7, 16, 'Paharo', 'Vent', NULL, 'Amical', 'Planeur'),
+(8, 17, 'Paharac', 'Vent', NULL, 'Caféiné', 'Fraternel'),
+(9, 18, 'Granpah', 'Vent', NULL, 'Persécuteur', 'HéroÏque'),
+(10, 21, 'Bunbun', 'Terre', 'Cristal', 'Caféiné', 'Vaillant'),
+(11, 22, 'Mudrid', 'Terre', 'Cristal', 'Réceptif', 'Résistant'),
+(12, 23, 'Hidody', 'Nature', NULL, 'Botaniste', 'Bioélectrique'),
+(13, 24, 'Taifu', 'Nature', NULL, 'Vaillant', 'Revanchard'),
+(14, 72, 'Crystle', 'Cristal', NULL, 'Amphibie', 'Reposé'),
+(15, 73, 'Sherald', 'Cristal', NULL, 'Réflecteur', 'Endurci');
 
 --
 -- Indexes for dumped tables
