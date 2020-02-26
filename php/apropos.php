19107e16c05e22b82911aa55aa8424fd02517419
Ivann <ivann.galic@edu.itescia.fr>
2020-02-26 21:06:36 +0100

    page à propos

--- php/apropos.php
+++ php/apropos.php
@@ -4,6 +4,14 @@
 
     <div class="container">
 
+        <p> Les Fondateurs du site :</p>
+        <div class="banniere_liste">
+            <img src="../imgs/laurent.png" alt="Liste des Temtem">
+            <img src="../imgs/kevin.png" alt="Liste des Temtem">
+            <img src="../imgs/ivann.png" alt="Liste des Temtem">
+        </div>
+        <p> < merci les gars pour cette semaine au top ! /></p>
+    </div>
         
 
 <?php
