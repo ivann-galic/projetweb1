2a17d34d39e84ed47ea56d4d950380fabdb3aa7f
Ivann <ivann.galic@edu.itescia.fr>
2020-02-27 11:23:08 +0100

    retouches home

--- php/home.php
+++ php/home.php
@@ -2,12 +2,19 @@
     include "../php/header_home.php";
 ?>
 
-    <div class="main-container">
+    <div class="first-container">
         <div class="container-video">
-            <iframe width="1000" height="595" src="https://www.youtube.com/embed/Csx5aK5mZCI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe width="600" height="357" src="https://www.youtube.com/embed/Csx5aK5mZCI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
         <div class="container-texte">
             <img src="../imgs/tableautxt.png" alt="Description du jeu">
+        </div>
+    </div>
+
+    <div class="second-container">
+        <div class="container-steam">
+            <img src="../imgs/steam.png" alt="Lien vers Steam">
+            <img src="../imgs/tempedie.png" alt="Lien vers le Tempedie">
         </div>
     </div>
 
